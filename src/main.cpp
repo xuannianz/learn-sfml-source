6f9d5d1fd94564a4b9f89a4b1d90e54809557dff
xuannianz <18051481300@163.com>
2022-07-12 21:43:07 +0800

    game timber

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,49 @@
+#include <algorithm>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
+#include <unistd.h>
 
-#include "SFML/Graphics/CircleShape.hpp"
-#include "SFML/Graphics/Color.hpp"
-#include "SFML/Graphics/Rect.hpp"
-#include "SFML/Graphics/RectangleShape.hpp"
-#include "SFML/Graphics/RenderStates.hpp"
-#include "SFML/Graphics/RenderWindow.hpp"
-#include "SFML/Graphics/Sprite.hpp"
-#include "SFML/Graphics/Texture.hpp"
-#include "SFML/Graphics/Font.hpp"
-#include "SFML/Graphics/Text.hpp"
-#include "SFML/System/Vector2.hpp"
+#include "SFML/Audio/SoundBuffer.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+#include "SFML/System/Sleep.hpp"
 #include "SFML/Window.hpp"
-#include "SFML/Window/Event.hpp"
-#include "SFML/Window/Keyboard.hpp"
-#include "SFML/Window/VideoMode.hpp"
-#include "SFML/Window/WindowStyle.hpp"
+#include "SFML/Audio.hpp"
 
 using namespace std;
 using namespace sf;
 namespace g1{
+    enum class Side{
+        NONE    = 0,
+        LEFT    = 1,
+        RIGHT   = 2,
+    };
+    void spawn_branches(Sprite *sprite_branchs, size_t n){
+        for (int i = 0; i < n; ++i){
+            int direction = rand() % 3;
+            int height = i * 150;
+            if (direction == static_cast<int>(Side::LEFT)){
+                sprite_branchs[i].setPosition(610, height);
+                sprite_branchs[i].setRotation(180);
+            } else if (direction == static_cast<int>(Side::RIGHT)){
+                sprite_branchs[i].setPosition(1330, height);
+                sprite_branchs[i].setRotation(0);
+            } else {
+                sprite_branchs[i].setPosition(3000, height);
+                sprite_branchs[i].setRotation(0);
+            }
+        }
+    }
+    void update_branchs(Sprite *sprite_branchs, size_t n){
+        for (int i = n - 1; i > 0; --i){
+            sprite_branchs[i].setPosition(sprite_branchs[i - 1].getPosition().x, i * 150);
+            sprite_branchs[i].setRotation(sprite_branchs[i - 1].getRotation());
+        }
+        spawn_branches(sprite_branchs, 1);
+    }
     void test(){
         // Style::Fullscreen 有点危险啊, 退不出来
         const float window_height = 1080;
@@ -78,19 +98,69 @@ namespace g1{
         time_bar.setPosition((window_width - time_bar_width) / 2.0f, (window_height - time_bar_height) + 5.f);
         float time_remaining = 10;
         float pixel_per_second = time_bar_width / time_remaining;
+        // branch
+        Texture texture_branch;
+        texture_branch.loadFromFile("01/graphics/branch.png");
+        Vector2u texture_branch_size = texture_branch.getSize();
+        const int NUM_BRANCHES = 5;
+        Sprite sprite_branches[NUM_BRANCHES];
+        for (int i = 0; i < NUM_BRANCHES; ++i){
+            sprite_branches[i].setTexture(texture_branch);
+            sprite_branches[i].setOrigin(texture_branch_size.x / 2.f, texture_branch_size.y / 2.f);
+        }
+        // player
+        Texture texture_player;
+        texture_player.loadFromFile("01/graphics/player.png");
+        Sprite sprite_player;
+        const float player_left_x = 580;
+        const float player_right_x = 1200;
+        const float player_y = 720;
+        Vector2u player_size = texture_player.getSize();
+        sprite_player.setTexture(texture_player);
+        sprite_player.setPosition(player_right_x, player_y);
+        // sprite_player.setOrigin(player_size.x / 2.f, player_size.y / 2.f);
+        // sprite_player.setPosition(player_right_x + player_size.x / 2.f, player_y + player_size.y / 2.f);
+        // gravestone
+        Texture texture_stone;
+        texture_stone.loadFromFile("01/graphics/rip.png");
+        Sprite sprite_stone;
+        sprite_stone.setTexture(texture_stone);
+        sprite_stone.setPosition(window_width, 860);
+        // axe
+        Texture texture_axe;
+        texture_axe.loadFromFile("01/graphics/axe.png");
+        Sprite sprite_axe;
+        const float axe_left_x = 700;
+        const float axe_right_x = 1075;
+        const float axe_y = 830;
+        sprite_axe.setTexture(texture_axe);
+        sprite_axe.setPosition(axe_right_x, axe_y);
+        // log
+        Texture texture_log;
+        texture_log.loadFromFile("01/graphics/log.png");
+        Sprite sprite_log;
+        sprite_log.setTexture(texture_log);
+        sprite_log.setPosition(810, 720);
+        // audio
+        SoundBuffer buf;
+        // buf.loadFromFile("/media/adam/18f44327-4098-4d8f-acd3-785433ccc1e1/Music/张信哲-白月光.flac");
+        buf.loadFromFile("/media/adam/18f44327-4098-4d8f-acd3-785433ccc1e1/Music/梁朝伟刘德华-无间道国语.wav");
+        // buf.loadFromFile("01/sound/chop.wav");
+        Sound sound;
+        sound.setBuffer(buf);
+        sound.play();
         Clock clock;
         bool paused = true;
-        while (window.isOpen())
-        {
+        bool accept_input = true;
+        while (window.isOpen()) {
             // check all the window's events that were triggered since the last iteration of the loop
             sf::Event event;
-            while (window.pollEvent(event))
-            {
+            while (window.pollEvent(event)) {
                 // "close requested" event: we close the window
                 if (event.type == sf::Event::Closed)
                     window.close();
                 
-                if (event.type == sf::Event::KeyPressed){
+                if (event.type == sf::Event::KeyPressed && accept_input){
                     if (Keyboard::isKeyPressed(Keyboard::Escape)){
                         window.close();
                     // 回车键, 开始游戏
@@ -99,7 +169,31 @@ namespace g1{
                         clock.restart();
                         score = 0;
                         time_remaining = 10;
+                        spawn_branches(sprite_branches, NUM_BRANCHES);
+                    } else if (Keyboard::isKeyPressed(Keyboard::Right) && !paused){
+                        ++score;
+                        time_remaining += (2.f / score) + 0.15f;
+                        sprite_axe.setPosition(axe_right_x, axe_y);
+                        sprite_player.setPosition(player_right_x, player_y);
+                        // sprite_player.setPosition(player_right_x + player_size.x / 2.f, player_y + player_size.y / 2.f);
+                        update_branchs(sprite_branches, NUM_BRANCHES);
+                        accept_input = false;
+                    } else if (Keyboard::isKeyPressed(Keyboard::Left) && !paused){
+                        ++score;
+                        time_remaining += (2.f / score) + 0.15f;
+                        sprite_axe.setPosition(axe_left_x, axe_y);
+                        sprite_player.setPosition(player_left_x, player_y);
+                        update_branchs(sprite_branches, NUM_BRANCHES);
+                        // sprite_player.setScale(-1, 1);
+                        // sprite_player.setPosition(player_left_x + player_size.x / 2.f, player_y + player_size.y / 2.f);
+                        // sprite_player.rotate(180);
+                        accept_input = false;
                     }
+                }
+                if (event.type == Event::KeyReleased){
+                    // if (event.key.code == Keyboard::Left ||
+                        // event.key.code == Keyboard::Right)
+                    accept_input = true;
                 }
             }
             if (!paused){
@@ -139,7 +233,6 @@ namespace g1{
                         cloud_isalive = false;
                     }
                 }
-                score = rand() % 100;
                 stringstream ss;
                 ss << "Score: " << score;
                 text_score.setString(ss.str());
@@ -163,8 +256,15 @@ namespace g1{
             window.draw(sprite_bee);
             window.draw(text_score);
             window.draw(time_bar);
+            window.draw(sprite_player);
+            window.draw(sprite_stone);
+            window.draw(sprite_axe);
+            window.draw(sprite_log);
             if (paused){
                 window.draw(text_msg);
+            }
+            for (int i = 0; i < NUM_BRANCHES; ++i){
+                window.draw(sprite_branches[i]);
             }
             window.display();
         }
